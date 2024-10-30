da619bff1dee57bd234e401ea626a1198145df02
alan nicolas <alanlimachivillarroel@gmail.com>
2024-10-30 01:47:29 -0400

    vista inventario

--- resources/views/inventarios/index.blade.php
+++ resources/views/inventarios/index.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventarios</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 <div class="encabezado">
             <a href="index.php">
             <div class="card">
-                <img src="multimedia/images/elements/farmacia.png">
+                <img src="{{ asset('iconos/farmacia.svg') }}">
                 <h1>San Agustin</h1>
             </div>
             </a>
@@ -18,83 +18,90 @@
   <li class="icon-content">
     <a href="index.php" aria-label="Spotify" data-social="spotify">
         <div class="filled"></div>
-        <i class="fa fa-box"></i>
+        <img src="{{ asset('iconos/inventario.svg') }}" alt="Inventarios">
     </a>
     <div class="tooltip">Inventarios</div>
   </li>
   <li class="icon-content">
-    <a href="php/viewbrand.php" aria-label="Pinterest" data-social="pinterest">
-      <div class="filled"></div>
-      <i class="fa-solid fa-tag"></i>
-    </a>
-    <div class="tooltip">Marcas</div>
-  </li>
-  <li class="icon-content">
     <a href="{{ route('producto.index') }}" aria-label="Dribbble" data-social="dribbble">
       <div class="filled"></div>
-      <i class="fas fa-pills"></i>
-      <div class="tooltip">Productos</div>
+      <img src="{{ asset('iconos/producto.svg') }}" alt="Productos">
     </a>
+    <div class="tooltip">Productos</div>
   </li>
   <li class="icon-content">
-    <a href="{{ route('lotes.index') }}" aria-label="Telegram" data-social="telegram">
+    <a href="{{ route('lote.index') }}" aria-label="Telegram" data-social="telegram">
       <div class="filled"></div>
-      <i class="fas fa-barcode"></i>
-      <div class="tooltip">Lotes</div>
+      <img src="{{ asset('iconos/lote.svg') }}" alt="Lotes">
+</a>
+<div class="tooltip">Lotes</div>
+  </li>
+  <li class="icon-content">
+    <a href="php/viewbrand.php" aria-label="Pinterest" data-social="pinterest">
+      <div class="filled"></div>
+      <img src="{{ asset('iconos/proveedor.svg') }}" alt="Proveedores">
     </a>
+    <div class="tooltip">Proveedores</div>
   </li>
 </ul>
         </div>
+
+    <div class="contenido">
     <div class="buscador">
         <form action="{{ route('inventario.search') }}" method="post">
             @csrf
-            @php
-                use App\Http\Controllers\InventarioController;
-                $columns = InventarioController::getAllColumnsInventario();
-            @endphp
-            @foreach ($columns as $column)
-                <input type="text" name="{{ $column }}" placeholder="{{ str_replace('_', ' ', $column) }}">
-                @if($column == 'ruta_imagen')
-                    <input type="file" name="{{ $column }}">
-                @elseif($column == 'proveedor_id')
-                    <select name="{{ $column }}">
-                        @foreach ($proveedores as $proveedor)
-                            <option value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
-                        @endforeach
-                    </select>
-                @endif
-            @endforeach
-            <input type="submit" value="Buscar">
+            <select name="search_column">
+                    <option value="nombre">Nombre</option>
+                    <option value="descripcion">Descripción</option>
+                    <option value="cantidad_total">Cantidad Total</option>
+                    <option value="valor_total">Valor Total</option>
+                </select>
+            <div class="input-container">
+                <input type="text" name="search_value" placeholder="Buscar...">
+                <button type="submit">
+                    <img src="{{ asset('iconos/lupa.svg') }}" alt="Buscar">
+                </button>
+            </div>
         </form>
     </div>
     
     @if(session('success'))
         <div>{{ session('success') }}</div>
     @endif
-
-    <style>
-        .inventario-link {
-            cursor: pointer;
-            text-decoration: none;
-            color: inherit;
-        }
-    </style>
-
-   
+    <div class="inventario">
     @foreach ($inventarios as $inventario)
-    <a href="{{ route('inventario.productos', $inventario->id) }}">
-        <div class="tarjeta">
-        <p><strong>Nombre:</strong> {{ $inventario->nombre }}</p>
-        <p><strong>Descripción:</strong> {{ $inventario->descripcion }}</p>
-        <p><strong>Cantidad Total:</strong> {{ $inventario->cantidad_total }}</p>
-        <p><strong>Valor Total:</strong> {{ $inventario->valor_total }}</p>
-        <img src="{{ asset('images/' . $inventario->ruta_imagen) }}" alt="{{ $inventario->nombre }}">
-        <a href="{{ route('inventario.edit', $inventario->id) }}"><button>Editar</button></a>
-        <a href="{{ route('inventario.delete', $inventario->id) }}"><button>Eliminar</button></a>
-        </div>
-    </a>
-    @endforeach
+ <a href="{{ route('inventario.productos', $inventario->id) }}">
+     <div class="card">
+     <div class="profile-pic"><img src="{{ asset('images/' . $inventario->ruta_imagen) }}" alt="{{ $inventario->nombre }}"></div>
+     <div class="bottom">
+         <div class="content">
+             <span class="name">{{ $inventario->nombre }}</span>
+             <span class="about-me">{{ $inventario->descripcion }}</span>
+             <div class="detalles">
+                 <div class="cantidad">
+                     <strong>{{$inventario->cantidad_total}}</strong>
+                     <span>Articulos</span>
+                 </div>
+             <div class="precio">
+                 <strong>{{$inventario->valor_total}} Bs</strong>
+                 <span>Total</span>
+             </div>
+         </div>
+         </div>
+         <div class="bottom-bottom">
+         <div class="botones">
+         <a href="{{ route('inventario.edit', $inventario->id) }}"><button class="btn open-modal-edit"><img src="{{ asset('iconos/editar.svg') }}" alt="Editar"></button></a>
+         <a href="{{ route('inventario.delete', $inventario->id) }}"><button class="btn open-modal-delete"><img src="{{ asset('iconos/eliminar.svg') }}" alt="Eliminar"></button></a>
+         </div>
+     </div>
+     </div>
+     </div>
+ </a>
+ @endforeach
 
-    <a href="{{ route('inventario.create') }}"><button>Agregar Inventario</button></a>
+ <a href="{{ route('inventario.create') }}"><button>Agregar Inventario</button></a>
+    </div>
+    </div>
+        
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ Route::get('/', function () {
 
 
 require __DIR__.'/inventario.route.php';
+require __DIR__.'/producto.route.php';
+require __DIR__.'/lote.route.php';
+require __DIR__.'/marca.route.php';
+require __DIR__.'/categoria.route.php';
+require __DIR__.'/proveedor.route.php';
